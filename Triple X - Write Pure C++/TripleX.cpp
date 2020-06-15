6a53d5364ac43b684e3616b2e8b555c27a733f01
Randal Quick <snoopisarmy@gmail.com>
2020-06-15 15:29:37 -0400

    Begin Work on Triple X

---
+++ Triple X - Write Pure C++/TripleX.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "You are a secret agent breaking into a secure server room...";
+    std::cout << std::endl;
+    std::cout << "Enter the correct code to continue...";
+
+    int a = 4;
+
+    std::cout << std::endl;
+    std::cout << a;
+    std::cout << std::endl;
+
+    return 0;
+}
